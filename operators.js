e27b7b6789632f018f6100563ef02986362ce36f
Chaitali1210 <160309331+Chaitali1210@users.noreply.github.com>
2024-03-03 09:23:30 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,51 @@
+let a = 78
+let b = 22
+//arithmetic operators
+// console.log("a+b=",a+b)
+// console.log("a-b=",a-b)
+// console.log("a*b=",a*b)
+// console.log("a/b=",a/b)
+// console.log("a%b=",a%b)
+// console.log("a**b=",a**b) //exponention 
+// console.log("a++=",a++) //incremental but after a is printed
+// console.log("a--=",a--)
+// console.log("++a=",++a)
+// console.log("--a=",--a)//disincremental immediately
+// console.log("a=",a)
+
+//assignment operators
+let assignment =2
+assignment += 7
+assignment -= 1
+assignment *= 1
+assignment /= 1
+assignment %= 1
+assignment **= 2
+console.log(assignment)
+
+
+// comparison operators
+let x =40
+let y =10
+
+console.log("x==y", x==y)
+console.log("x!=y",x!=y)
+console.log("x===y", x===y)
+console.log("x!==y", x!==y)
+console.log("x>y", x>y)
+console.log("x<y", x<y)
+console.log("x>=y", x>=y)
+console.log("x<=y", x<=y)
+
+
+//logical operators
+
+let no = 5
+let no1= 8
+console.log(no<no1 && no==5) // logical and gives true o/p if the value is true
+console.log(no>no1 || no==5) // logical or/ gives false o/p for true and vica versa
+console.log(!false) // logical not/ gives false value true
+console.log(!true) // logical not/ gives true value false
+
+
+
---
+++ variables.js
@@ -0,0 +1,72 @@
+// var name= "chaitali"
+// console.log(name);
+
+// let a = 67
+// console.log(a)
+
+// const name= "chaitali"
+// // console.log(name)
+
+// var a = 45
+// var b = "javascript"
+// var c = null
+// var d = undefined
+// {
+//     var b = 'this'
+//     console.log(b)
+// }
+
+
+// // console.log(b)
+
+
+// var a = 45
+// let b = "javascript"
+// var c = null
+// var d = undefined
+// {
+    
+//     let b = 'this' //let is blockscoped/
+//     console.log(b)
+// }
+
+
+// console.log(b)
+
+
+
+// let place = "vasai-virar"
+// console.log(place)
+// console.log(typeof(place))
+
+// let time = 12
+// console.log(time)
+// console.log(typeof(time))
+
+// let a = 34
+// let b = 64
+// // console.log(a+b)
+
+// const car = {
+//     brand :"hyundai",
+//     price : '15 lacs',
+//     disel: "yes",
+//     insurace: 2000
+// }
+// car['model']="verna"
+// car['disel']="false"
+// console.log(car)
+// console.log(typeof(car))
+
+
+// const dict = {
+//     adumbrate:"to foreshadow; prefigure.",
+//     Anachronism:" Something out of place for the time period",
+//     Artless:"Entirely and completely.",
+//     Asperity:" Without deception.",
+//     Belie:"Harsh in tone."
+// }
+// console.log(dict)
+// console.log(dict.Belie)
+// console.log(dict['Artless'])
+
